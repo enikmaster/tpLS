6599b9bb046097447594a6aeb6b4f8f57f354e7c
Filipe Carvalho <ftcarvalho@gmail.com>
2022-06-07 19:01:49 +0100

    pequeno erro

--- sopa/src/components/ListaWords.js
+++ sopa/src/components/ListaWords.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
 import Card from './Card';
 import './css/listaWords.css';
 import { palavras } from "../data";
 
 function ListaWords(props) {
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    }
+   
 
     return (
         <Card className="listaWords-container">
             <h3>Lista de Palavras</h3>
-            <button onClick={clickHandler}>Start Game</button>
+            
             <h1>{title}</h1>
             <div>
                 {palavras.map((data, key) => {
