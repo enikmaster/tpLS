1e92491accd4ac079005c9c38aed28476a3f482f
lourenco <mcbride9696@gmail.com>
2022-06-07 19:13:50 +0100

    Update

---
+++ sopa/src/components/menuInicial.js
@@ -0,0 +1,19 @@
+import './css/body.css'
+
+function MenuInicial() {
+    return (
+        <div>
+            <h3> Menu </h3>
+            <div className="MenuInicial">
+                <button className="IniciaJogo"> Iniciar Jogo</button>
+                <button className="HighScore"> Ver Pontuações</button>
+
+                
+            </div>
+
+        </div>
+    )
+}
+
+
+export default MenuInicial;
