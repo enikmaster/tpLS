5c1f6c07da14010936eef5bd53e4081040e61dd6
lourenco <mcbride9696@gmail.com>
2022-06-10 18:31:22 +0100

    gridupdated

--- sopa/src/components/Jogar.js
+++ sopa/src/components/Jogar.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 function Jogar() {
   var i = 0;
   var x;
+  var gridN="";
   var dificuladade = 10;
   var facil = 10;
   var medio = 5;
@@ -12,22 +13,26 @@ function Jogar() {
 
   if (dificuladade === facil) {
     x = 10;
+    gridN="DivGrid10";
   }
-
+  
   if (dificuladade === medio) {
     x = 15;
+    gridN="DivGrid15";
   }
-
+  
   if (dificuladade === dificil) {
     x = 20;
+    gridN="DivGrid20";
   }
 
   for (i = 0; i < x * x; i++) {
     s.push(i);
   }
 
+
   return(
-     <div className="DivGrid">
+     <div className={gridN}>
         {s.map((items)=>(
             <div id = {items} className = "inGrid">
                {items} </div>
