e472fe37a964b837ea31854bd7976b917d1d17b1
Diogo <diogofgf97@gmail.com>
2022-06-14 14:20:05 +0100

    AlteracoesSeletor

--- sopa/src/components/Jogar.js
+++ sopa/src/components/Jogar.js
@@ -1,27 +1,28 @@
 import './css/divGrid.css'
 import { useState } from "react";
 
-function Jogar() {
-  var i = 0;
-  var x;
-  var gridN="";
-  var dificuladade = 10;
-  var facil = 10;
-  var medio = 5;
-  var dificil = 20;
-  var s = [];
-
-  if (dificuladade === facil) {
+function Jogar(props) {
+  const {selectedLevel} = props;
+  
+  let i = 0;
+  let x;
+  let gridN="";
+  let facil = 1;
+  let medio = 2;
+  let dificil = 3;
+  let s = [];
+  console.log("console log: " + selectedLevel.value)
+  if (selectedLevel === facil) {
     x = 10;
     gridN="DivGrid10";
   }
   
-  if (dificuladade === medio) {
+  if (selectedLevel === medio) {
     x = 15;
     gridN="DivGrid15";
   }
   
-  if (dificuladade === dificil) {
+  if (selectedLevel === dificil) {
     x = 20;
     gridN="DivGrid20";
   }
