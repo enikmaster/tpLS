27882a52da705d48a3d50538480a45142a239561
mcbride96 <mcbride9696@gmail.com>
2022-06-16 15:28:44 +0100

    adicionado lista de palavras

--- sopa/src/components/Jogar.js
+++ sopa/src/components/Jogar.js
@@ -6,21 +6,31 @@ function Jogar(props) {
   let i = 0;
   let s = [];
   let x = props.gridN;
-  let names=['Jenna', 'Johnny', 'Erica', 'Amy', 'Steve', 'Julia', 'Tony', 'Alexis', 'James', 'Clara', 'Mark', 'Michelle'];
+  let names = ['Jenna', 'Johnny', 'Erica', 'Amy', 'Steve', 'Julia', 'Tony', 'Alexis', 'James', 'Clara', 'Mark', 'Michelle'];
+  let listWords = names.map((items) =>
+    <li>{items}</li>
+  );
 
   for (i = 0; i < x * x; i++) {
     s.push(i);
   }
-  // "Ola tenho " + gridN + ' anos'
-  // `ola tenho ${gridN} anos`
+
   return (
-    <div className={`DivGrid${x} hidden`}>
-      {s.map((items) => (
-        <div id={items} className="inGrid">
-          0{" "}
-        </div>
-      ))}
-    </div>
+    <div className="main_div">
+      <div className={`DivGrid${x} hidden`}>
+
+        {s.map((items) => (
+          <div id={items} className="inGrid">
+            0{" "}
+          </div>
+        ))}
+      </div>
+      <div className="sidemenu">
+        <ul>{listWords}</ul>
+      </div>
+
+
+    </div >
   );
 }
 
