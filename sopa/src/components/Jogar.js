fed31f0aa1845ca8dfefe2a77b97c6b432301f03
mcbride96 <mcbride9696@gmail.com>
2022-06-18 23:14:32 +0100

    palavrasjaescrevem

--- sopa/src/components/Jogar.js
+++ sopa/src/components/Jogar.js
@@ -9,7 +9,7 @@ const Jogar = (props) => {
   let jr = 0;
   let s = [];
   let arr = [];
-  let comp =[];
+  let comp = [];
   let o;
   let x = props.gridN;
 
@@ -17,7 +17,6 @@ const Jogar = (props) => {
   const letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const letrasSize = letras.length;
   let namesAux = names;
-  let namesSize = names.length;
   let listWords = names.map((items) =>
     <li>{items}</li>
   );
@@ -30,13 +29,13 @@ const Jogar = (props) => {
   }
   for (i = 0; i < x * x; i++) {
     let n = letra();
-    s.push(n);
+    s.push(i);
   }
   //cria lista 
   escolheListaPalavras(x);
   function escolheListaPalavras(x) {
-    let xc = namesSize;
-    for (i = 0; i < namesSize - 5; i++) {
+    let xc = names.length - 5;
+    for (i = 0; i < names.length - 5; i++) {
       let n = Math.floor(Math.random() * xc);
       let k = namesAux.splice(n, 1);
       xc--;
@@ -77,55 +76,212 @@ const Jogar = (props) => {
     return [...word].reverse().join("")
   }
 
-  //funcao para escrever a palavra no index random 
-  function verificaOndeEscreverPalavra(size, column, l, word) {
+  //ESCREVE HORIZONTAL E->D
+  function verificalinhad(size, l, word, sc) {
     let m = l;
-    let sc = size + column;
-   
-   /* for(i=0; i<word.length; i++){
-      comp[i]=s[m];
-      m++;
-    }*/
-   // console.log(comp);
+    let verify = 0;
+    let linhad = 0;
+    i = 0;
+    if (Number.isInteger(s[l]) == true || s[l] == word[i]) {
+      verify = 1;
+    }
+    if (verify != 1) {
+      return 0;
+    }
     if (sc <= x) {
-      m=l;
-      for (i = 0; i < word.length; i++) {
-        s[m] = word[i];
-        m++;
-      }
-      criarDoisDArray(arr);
-      return;
-    }
-    if ((column - size) % x > 0) {
-      m=l;
-      let tam = size;
-      word = reverse(word);
-      console.log(word);
       for (i = 0; i < size; i++) {
-        s[m-tam] = word[i];
-         tam--;
+        if (Number.isInteger(s[l + i]) == true) {
+          linhad++;
+        }
+        else if (s[l + i] == word[i]) {
+          linhad++;
+        }
       }
-      criarDoisDArray(arr);
-      return;
+      if (linhad == size) {
+        m = l;
+        for (i = 0; i < size; i++) {
+          s[m] = word[i];
+          m++;
+        }
+        criarDoisDArray(arr);
+        return 1;
+      }
+      else
+        return 0;
     }
-    return;
+
+    return 0;
+  }
+  //ESCREVE HORIZONTAL D>E
+  function verificalinhae(size, column, l, word, sc) {
+    let m = l;
+    let verify = 0;
+    let linhae = 0;
+    i = 0;
+    if (Number.isInteger(s[l]) == true || s[l] == word[i]) {
+      verify = 1;
+    }
+    if (verify != 1) {
+      return 0;
+    }
+    if (column - size >= 0) {
+      m = l;
+      for (i = 0; i < size; i++) {
+        if (Number.isInteger(s[m--]) == true) {
+          linhae++;
+        }
+        else if (s[m--] == word[i]) {
+          linhae++;
+        }
+      }
+      if (linhae == size) {
+        m = l;
+        for (i = 0; i < word.length; i++) {
+          s[m--] = word[i];
+        }
+        criarDoisDArray(arr);
+        return 1;
+      }
+      else
+        return 0;
+    }
+    return 0;
+  }
+
+  //ESCREVE VERTICAL C->B
+  function verificac(size, l, word, row) {
+    let m = l;
+    let verify = 0;
+    let linhac = 0;
+    let valorx = 0;
+    if (Number.isInteger(s[l]) == true || s[l] == word[i]) {
+      verify = 1;
+    }
+    if (verify != 1) {
+      return 0;
+    }
+    if (size + row <= x) {
+      //console.log("Verify: " + verify);
+      for (i = 0; i < size; i++) {
+        if (Number.isInteger(s[l + valorx]) == true) {
+          linhac++;
+          valorx += x;
+        }
+        else if (s[l + valorx] == word[i]) {
+          linhac++;
+          valorx += valorx;
+        }
+      }
+      if (linhac == size) {
+        m = l;
+        valorx = 0;
+        for (i = 0; i < size; i++) {
+          s[l + valorx] = word[i];
+          valorx += x;
+        }
+        criarDoisDArray(arr);
+        return 1;
+      }
+      else
+        return 0;
+      return 0;
+    }
+    return 0;
 
   }
+
+  //ESCREVE VERTICAL B->C
+  function verificab(size, l, word, row) {
+    let m = l;
+    let verify = 0;
+    let linhac = 0;
+    let valorx = 0;
+    if (Number.isInteger(s[l]) == true || s[l] == word[i]) {
+      verify = 1;
+    }
+    if (verify != 1) {
+      return 0;
+    }
+    if (row - size >= 0) {
+      console.log("ERROS:::: " + verify);
+      for (i = 0; i < size; i++) {
+        if (Number.isInteger(s[l - valorx]) == true) {
+          linhac++;
+          valorx += x;
+        }
+        else if (s[l - valorx] == word[i]) {
+          linhac++;
+          valorx += valorx;
+        }
+      }
+      if (linhac == size) {
+        valorx = 0;
+        for (i = 0; i < size; i++) {
+          s[l - valorx] = word[i];
+          valorx += x;
+        }
+        criarDoisDArray(arr);
+        return 1;
+      }
+      else
+        return 0;
+    }
+    return 0;
+  }
+
+
   //serve para escolheraleatoriamente o index para cada palavra
   escolhePosicaoParaPalavra();
   function escolhePosicaoParaPalavra() {
     criarDoisDArray(arr);
     let f = 0;
+    let ver = 1;
+    let size;
+    let word;
+    let row;
+    let column;
+    let sc;
+    let l;
+    let random;
     for (f = 0; f < namesAux.length; f++) {
-      let l = Math.floor(Math.random() * (x * x));
-      let size = namesAux[f].length;
-      let word = namesAux[f];
-      let row = Math.floor(l / x);
-      let column = verificaColuna(row, l);
-      verificaOndeEscreverPalavra(size, column, l, word);
+
+      do {
+        l = Math.floor(Math.random() * (x * x));
+        size = namesAux[f].length;
+        word = namesAux[f];
+        row = Math.floor(l / x);
+        column = verificaColuna(row, l);
+        sc = column + size;
+        //ver = verificalinhad(size, l, word, sc);
+        random = Math.floor(Math.random() * (4));
+        switch (random) {
+          case 0:
+            ver = verificalinhad(size, l, word, sc);
+            break;
+          case 1:
+            ver = verificalinhae(size, column, l, word, sc);
+            break;
+          case 2:
+            ver = verificac(size, l, word, row);
+            break;
+          case 3:
+            ver = verificab(size, l, word, row);
+            break;
+          /*
+*/
+          default:
+            break;
+        }
+      } while (ver === 0);
+      console.log("Valor de random: " + random + " Linha: " + row + " Coluna: " + column + " Palavra: " + word + " VALOR DE L " + l);
+    }
+
+    for (i = 0; i < x * x; i++) {
+      if (Number.isInteger(s[i]) == true) {
+        s[i] = letra();
+      }
     }
   }
-
 
   return (
     <div className="tabuleiro">
@@ -137,12 +293,12 @@ const Jogar = (props) => {
         </div>
         <div className={`DivGrid${x}`}>
 
-        {s.map((items) => (
-          <div id={jr++} className="inGrid">
-            {items}{" "}
-          </div>
-        ))}
-      </div>
+          {s.map((items) => (
+            <div id={jr++} className="inGrid">
+              {items}{" "}
+            </div>
+          ))}
+        </div>
       </div>
       <ListaWords />
       <ListaTopTen />
